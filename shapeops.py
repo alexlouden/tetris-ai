14d355e4cc0c87a766e4c305bd8879229add7280
Alex Louden <alex@louden.com>
2013-04-28 14:05:00 +0800

    Finished all shape coordinates

--- shapeops.py
+++ shapeops.py
@@ -18,12 +18,12 @@ piece_shapes = {
     3: [(0,0), (0,3), (1,3), (1,2), (2,2), (2,1), (1,1), (1,0), (0,0)],
     4: [(0,0), (0,3), (2,3), (2,2), (1,2), (1,0), (0,0)],
     5: [(0,2), (0,3), (2,3), (2,0), (1,0), (1,2), (0,2)],
-
-
+    6: [(0,1), (0,3), (1,3), (1,2), (2,2), (2,0), (1,0), (1,1), (0,1)],
+    7: [(0,0), (0,2), (1,2), (1,3), (2,3), (2,1), (1,1), (1,0), (0,0)],
 }
 
 def get_shape_polygon(number):
-    return Polygon(piece_shapes.get(number, piece_shapes.get(2)))
+    return Polygon(piece_shapes[number])
 
 piece_colours = {
     1: '#ff0000',
@@ -36,4 +36,4 @@ piece_colours = {
 }
 
 def get_piece_colour(number):
-    return piece_colours.get(number)
+    return piece_colours[number]
