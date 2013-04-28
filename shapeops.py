27e4f77a5cd4d63d21b9a53b743a1ee03b1d9fee
Alex Louden <alex@louden.com>
2013-04-28 13:45:14 +0800

    Added shape colours and started on shapes

--- shapeops.py
+++ shapeops.py
@@ -11,8 +11,24 @@
 
 from shapely.geometry import Polygon
 
+piece_shapes = {
+    1: [(0,0), (1,0), (1,4), (0,4), (0,0)],
+    2: [(0,0), (2,0), (2,2), (0,2), (0,0)],
+
+}
+
 def get_shape_polygon(number):
-    return Polygon([(0,0), (2,0), (2,2), (0,2), (0,0)])
+    return Polygon(piece_shapes.get(number))
+
+piece_colours = {
+    1: '#ff0000',
+    2: '#de60cc',
+    3: '#f79646',
+    4: '#ffff00',
+    5: '#00b050',
+    6: '#66ffff',
+    7: '#4f81bd'
+}
 
 def get_piece_colour(number):
-    return '#FF0000'
+    return piece_colours.get(number)
