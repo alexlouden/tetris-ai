01df70c8e6bc5709a98ab14ca15109695d3d1169
Alex Louden <alex@louden.com>
2013-05-19 17:19:24 +0800

    Progress towards a tree (albiet shakey). TODO: fix get_best_moves()

--- ai.py
+++ ai.py
@@ -13,10 +13,12 @@
 
 from copy import deepcopy
 from pprint import pprint
+from operator import attrgetter
+
 from shapeops import num_useful_rotations
 from plotting import plot_game
 
-max_iteration_cost = 5
+max_iteration_cost = 10
 """ The maximum cost that we'll explore subsequent moves for """
 
 class Weightings(object):
@@ -40,14 +42,16 @@ class Move(object):
         self.left = left
 
         # Store cost - use .calculate_cost()
-        self._cost = None
+        self.cost = None
 
         self.stats = Stats()
 
+        self.next_moves = []
+
     def __str__(self):
-        if self._cost is not None:
+        if self.cost is not None:
             return "<Move: {0.piece} rot:{0.piece.rotation}"\
-                " left:{0.left} cost:{0._cost:.2f}>".format(self)
+                " left:{0.left} cost:{0.cost:.2f}>".format(self)
         else:
             return "<Move: {0.piece} rot:{0.piece.rotation} left:{0.left}>".format(self)
 
@@ -66,9 +70,9 @@ class Move(object):
         num_gaps = self.game.count_gaps()
         height = self.game.height
 
-        name = 'game_move_{0.piece.id}_{0.left}_{0.piece.rotation}'.format(self)
-        self.game.status = name
-        plot_game(self.game, name)
+##        name = 'scenario\\game_move_{0.piece.id}_{0.piece.rotation}_{0.left}'.format(self)
+##        self.game.status = name
+##        plot_game(self.game, name)
 
         # Remove piece
         self.game.pieces.pop()
@@ -82,7 +86,8 @@ class Move(object):
         self.stats.gaps         = num_gaps - previous_num_gaps
         self.stats.height       = height - previous_height
 
-        print self.stats.__dict__
+##        print str(self)
+##        print self.stats.__dict__
 
 
     def calculate_cost(self, weighting):
@@ -94,7 +99,7 @@ class Move(object):
         cost += weighting.gaps         * self.stats.gaps
         cost += weighting.height       * self.stats.height
 
-        self._cost = cost
+        self.cost = cost
 
 
 def get_best_moves(game):
@@ -111,16 +116,28 @@ def get_best_moves(game):
     while game.input_queue:
 
         piece = game.input_queue.pop()
-
-        rotations = useful_rotations[piece.num]
-        possible_moves = get_possible_moves(game, piece, rotations)
-
-        for pm in possible_moves:
-            pm.try_dropping()
-            pm.calculate_cost(weights)
+        best_by_cost = get_moves_and_weights(game, piece, weights, useful_rotations)
 
         print 'Possible moves for piece {}:'.format(piece.id)
-        pprint (possible_moves)
+##        pprint (best_by_cost)
+
+
+        # TODO fix loops and remove repeated code
+
+        piece = game.input_queue.pop()
+
+        for move in best_by_cost:
+            game.drop(move.piece, move.left)
+
+            # Do next level
+            move.next_moves = get_moves_and_weights(game, piece, weights, useful_rotations)
+
+            game.pieces.pop()
+
+
+
+        pprint (best_by_cost)
+
 
 ##        moves.append(Move(p, rotation, left))
 
@@ -142,3 +159,17 @@ def get_possible_moves(game, piece, rotations):
             possible_moves.append(m)
 
     return possible_moves
+
+def get_moves_and_weights(game, piece, weights, useful_rotations):
+
+    rotations = useful_rotations[piece.num]
+    possible_moves = get_possible_moves(game, piece, rotations)
+
+    for pm in possible_moves:
+        pm.try_dropping()
+        pm.calculate_cost(weights)
+
+    # Sort possible moves by cost (best first)
+    best_by_cost = sorted(possible_moves, key=attrgetter('cost'))
+
+    return best_by_cost
--- test_scenario.py
+++ test_scenario.py
@@ -17,25 +17,46 @@ from nose.tools import assert_equals, assert_true
 from tetris import TetrisGame, TetrisPiece
 from plotting import plot_game
 
-def test_scenario_1():
+
+##def test_scenario_1():
+##
+##    pieces = [
+##        TetrisPiece(5, 'J'),
+##        TetrisPiece(4, 'L'),
+##        TetrisPiece(2, 'O')
+##    ]
+##
+##    # Initialise game with list of pieces
+##    game = TetrisGame(pieces, width=6)
+##    game.solve()
+##    game.status = "Scenario 1"
+##    print 'height:', game.height
+##
+####    # Check game height is 3
+####    assert_equals(game.height, 3)
+##
+##    plot_game(game, 'test_scenario_1')
+
+
+def test_scenario_2():
 
     pieces = [
-        TetrisPiece(5, 'J'),
-        TetrisPiece(4, 'L'),
-        TetrisPiece(2, 'O')
+        TetrisPiece(1, 'I'),
+        TetrisPiece(3, 'T'),
+        TetrisPiece(6, 'S'),
+        TetrisPiece(7, 'Z')
     ]
 
     # Initialise game with list of pieces
     game = TetrisGame(pieces, width=6)
     game.solve()
-    game.status = "Scenario 1"
+    game.status = "Scenario 2"
     print 'height:', game.height
 
 ##    # Check game height is 3
 ##    assert_equals(game.height, 3)
 
-    plot_game(game, 'test_scenario_1')
-
+    plot_game(game, 'test_scenario_2')
 
 
 if __name__ == '__main__':
