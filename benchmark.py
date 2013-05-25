f54619d9ea9f06bdb7ea227ae996ae55f1f21da9
Alex Louden <alex@louden.com>
2013-05-25 21:59:24 +0800

    Benchmark 10 random sets of pieces

--- benchmark.py
+++ benchmark.py
@@ -14,30 +14,37 @@
 import os
 from time import clock, time
 from random import randint
+from copy import deepcopy
+from pprint import pprint
 
 # Choose higher resolution time counter
 time = clock if os.name == 'nt' else time
 
 from tetris import TetrisGame, TetrisPiece
 from plotting import plot_game
+from ai import Weightings
 
 def main():
 
-##    for num_pieces in range(1, 10):
+    # 10 random sets of pieces
+    for i in range(10):
 
-    print 'num_pieces,time'
+        pieces = [ TetrisPiece(randint(1, 7), i) for i in range(0, 7)]
+        pprint(pieces)
 
+        piece_delays = []
 
-    num_pieces = 0
-    delay = time_solve(num_pieces)
+        for num_pieces in range(1, len(pieces)):
+            delay = time_solve(deepcopy(pieces[:num_pieces]), num_pieces)
+            piece_delays.append((num_pieces, delay))
 
-    print delay
+        print '-'*40
+        print 'num_pieces time'
+        pprint(piece_delays)
 
-def time_solve(num_pieces):
+def time_solve(pieces, num_pieces):
 
     start_time = time()
-
-    pieces = [ TetrisPiece(randint(1, 7), i) for i in range(0, 7)]
 
     # Initialise game with list of pieces
     game = TetrisGame(pieces, width=7)
@@ -45,7 +52,6 @@ def time_solve(num_pieces):
     game.solve()
 
     end_time = time()
-
     return end_time - start_time
 
 if __name__ == '__main__':
